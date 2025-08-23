d7faf061070f0f8548a60e3b3c7b07fca094b91d
luifer <info@hunt-tickets.com>
2025-08-23 07:22:23 -0500

    unew routes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        accept: 'accept.html'
+      }
+    }
+  },
   preview: {
     allowedHosts: ['hunt-refund-production.up.railway.app']
   }
